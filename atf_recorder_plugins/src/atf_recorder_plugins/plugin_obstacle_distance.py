82645272d2e3fb690e7ac9c15faad27183a81e36
Florian Köhler <florian.koehler@gmx.net>
2015-12-04 18:07:52 +0100

    added function to create testblock list

--- atf_recorder_plugins/src/atf_recorder_plugins/plugin_obstacle_distance.py
+++ atf_recorder_plugins/src/atf_recorder_plugins/plugin_obstacle_distance.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-import rospy
-
 from copy import copy
+
+import rospy
 from atf_recorder import BagfileWriter
 from obstacle_distance.srv import GetObstacleDistance
 
@@ -13,7 +13,10 @@ class RecordObstacleDistance:
         self.robot_config_file = robot_config_file
 
         resources_timer_frequency = 100.0  # Hz
-        self.timer_interval = 1/resources_timer_frequency
+        self.timer_interval = 1 / resources_timer_frequency
+
+        self.testblock_list = self.create_testblock_list()
+        self.requested_objects = {}
         self.res_pipeline = {}
 
         self.BfW = BagfileWriter(bag_file, write_lock)
@@ -32,6 +35,32 @@ class RecordObstacleDistance:
     def collect_obstacle_distances(self, event):
         pipeline = copy(self.res_pipeline)
         if not len(pipeline) == 0:
+            pass
+            # self.BfW.write_to_bagfile(topic, msg, rospy.Time.from_sec(time.time()))
 
+    def create_testblock_list(self):
 
-            self.BfW.write_to_bagfile(topic, msg, rospy.Time.from_sec(time.time()))
+        testblock_list = {}
+        for testblock in self.test_config:
+            try:
+                self.test_config[testblock]["obstacle_distance"]
+            except KeyError:
+                continue
+            else:
+                for item in self.test_config[testblock]["obstacle_distance"]:
+                    if len(item) == 1:
+                        # Single link or link chain to all objects
+                        try:
+                            testblock_list[testblock]
+                        except KeyError:
+                            testblock_list.update({testblock: {"all": []}})
+                        testblock_list[testblock]["all"].append(item[0])
+                    elif len(item) == 2:
+                        # Single link or link chain to one or more objects
+                        for object_name in item[1]:
+                            try:
+                                testblock_list[testblock][object_name]
+                            except KeyError:
+                                testblock_list[testblock].update({object_name: []})
+                            testblock_list[testblock][object_name].append(item[0])
+        return testblock_list
